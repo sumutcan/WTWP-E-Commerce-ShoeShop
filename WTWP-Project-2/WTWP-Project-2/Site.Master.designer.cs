e11629111cba0ae133a6ad293e0d527f106e3049
Umutcan Simsek <s.umutcan@gmail.com>
2012-05-22 04:31:32 +0300

    sitemappath tamam

--- WTWP-Project-2/WTWP-Project-2/Site.Master.designer.cs
+++ WTWP-Project-2/WTWP-Project-2/Site.Master.designer.cs
@@ -121,6 +121,15 @@ namespace WTWP_Project_2 {
         protected global::System.Web.UI.ScriptManager ScriptManager1;
         
         /// <summary>
+        /// SiteMapPath1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SiteMapPath SiteMapPath1;
+        
+        /// <summary>
         /// pnlHata control.
         /// </summary>
         /// <remarks>
